f412b76af8bdeb26571bc90d16ebe2a9aa3d74c1
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-02-23 21:39:55 -0300

    tela inicial do feedbacker

--- dashboard/src/views/Home/index.vue
+++ dashboard/src/views/Home/index.vue
@@ -1,9 +1,44 @@
 <template>
-  <h1 class="font-black text-brand-main">Aqui é a Home</h1>
+  <CustomHeader
+    @create-account="handlerAccountCreate"
+    @login="handlerLogin"
+  />
+  <Contact/>
+  <div class="flex justify-center py-10 bg-brand-gray">
+    <p class="font-medium text-center text-gray-800">feedbacker &#169; 2012</p>
+  </div>
 </template>
 
 <script>
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+import CustomHeader from '@/views/Home/CustomHeader'
+import Contact from '@/views/Home/Contact'
+
 export default {
-  name: 'index'
+  components: { Contact, CustomHeader },
+  setup () {
+    const router = useRouter()
+
+    onMounted(() => {
+      const token = window.localStorage.getItem('token')
+      if (token) {
+        router.push({ name: 'Feedbacks' })
+      }
+    })
+
+    function handlerLogin () {
+
+    }
+
+    function handlerAccountCreate () {
+
+    }
+
+    return {
+      handlerLogin,
+      handlerAccountCreate
+    }
+  }
 }
 </script>
