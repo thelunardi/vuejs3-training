3a9071fb637984ad5e5a57d2fe0b1db46c4215f4
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-02-27 11:42:55 -0300

    pegar o usuario global e controlar rotas autenticadas

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -4,11 +4,31 @@
 </template>
 
 <script>
+import { watch } from 'vue'
 import ModalFactory from '@/components/ModalFactory'
+import { useRoute, useRouter } from 'vue-router'
+import services from './services'
 
 export default {
   components: {
     ModalFactory
+  },
+  setup () {
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+        const { data } = await services.users.getMe()
+        console.log(data)
+      }
+    })
   }
 }
 </script>
