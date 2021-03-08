c4629bdf2f657d59e9200b03db74eed21f173663
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-03-08 20:28:39 -0300

    setup do widget

---
+++ widget/src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@/assets/css/fonts.css'
+import '@/assets/css/tailwind.css'
+import 'animate.css'
+
+createApp(App).mount('#app')
