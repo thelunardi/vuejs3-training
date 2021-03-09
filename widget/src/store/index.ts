752332cec5fb0f6fa2e3cff1c391fc1af21bbe12
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-03-09 20:23:39 -0300

    adicionando a store no projeto

---
+++ widget/src/store/index.ts
@@ -0,0 +1,60 @@
+import { reactive, readonly } from 'vue'
+
+export type StoreState = {
+  currentComponent: string,
+  message: string,
+  feedbackType: string,
+  fingerprint: string,
+  apiKey: string,
+  currentPage: string
+}
+
+const initialState: StoreState = {
+  currentComponent: 'SelectFeedbackType',
+  message: '',
+  feedbackType: '',
+  fingerprint: '',
+  apiKey: '',
+  currentPage: ''
+}
+
+const state = reactive<StoreState>({ ...initialState })
+
+export function setCurrentComponent(component: string) {
+  state.currentComponent = component
+}
+
+export function setMessage(message: string) {
+  state.message = message
+}
+
+export function setFeedbackType(type: string) {
+  state.feedbackType = type
+}
+
+export function setCurrentPage(page: string) {
+  state.currentPage = page
+}
+
+export function setApiKey(apiKey: string) {
+  state.apiKey = apiKey
+}
+
+export function setFingerprint(fingerprint: string) {
+  state.fingerprint = fingerprint
+}
+
+export function resetStore () {
+  setCurrentComponent(initialState.currentComponent)
+  setMessage(initialState.message)
+  setFeedbackType(initialState.feedbackType)
+  setCurrentPage(initialState.currentPage)
+  setApiKey(initialState.apiKey)
+  setFingerprint(initialState.fingerprint)
+}
+
+export default readonly(state)
+
+
+
+
