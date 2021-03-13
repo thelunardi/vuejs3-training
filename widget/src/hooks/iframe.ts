58edd1c42b05a6d6fe970df577b0af8d21702e8e
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2021-03-13 10:47:30 -0300

    alterando endereço do widget - valor da apikey

--- widget/src/hooks/iframe.ts
+++ widget/src/hooks/iframe.ts
@@ -20,7 +20,7 @@ export default function useIFrameControl (): FrameControlInterface {
       return
     }
 
-    setApiKey('fcd5015c-10d3-4e9c-b395-ec7ed8850165')
+    setApiKey('9224605e-5960-423e-ab7e-52c5307a5746')
     setCurrentPage('http://teste.com')
     setFingerprint('123123123123')
     console.log('aqwe')
